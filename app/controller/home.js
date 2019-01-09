28ab3c913ce9bd22b188101712057e95fd987783
xubowen <bowen.xu@bangdao-tech.com>
2019-01-09 21:06:04 +0800

    :tada:

---
+++ app/controller/home.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const title = '我是首页'; // 向模板传入数据
+    await this.ctx.render('index', {
+      title,
+    });
+  }
+}
+
+module.exports = HomeController;
